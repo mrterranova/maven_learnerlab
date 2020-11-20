fe2ab17c53c474a633d633766c4eabc469d8a1e1
Michal Terranova <michalterranova@mail.com>
2020-11-20 14:49:15 -0500

    Update TestPerson.java

--- src/test/java/com/github/curriculeon/TestPerson.java
+++ src/test/java/com/github/curriculeon/TestPerson.java
@@ -1,5 +1,36 @@
 package com.github.curriculeon;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestPerson {
 
+    @Test
+    public void testConstructor(){
+        // : Given
+        long expectedId = Long.MAX_VALUE;
+        String expectedName = "Michal";
+        // : When
+        Person person = new Person(expectedId, expectedName);
+
+        //
+        Assert.assertEquals(expectedId, person.getId());
+        Assert.assertEquals(expectedName, person.getName());
+    }
+
+    @Test
+    public void testSetName(){
+        // : Given
+        long expectedId = Long.MAX_VALUE;
+        String expectedName = "Michal";
+        // : When
+        Person person = new Person(expectedId, expectedName);
+
+        //
+        Assert.assertEquals(expectedId, person.getId());
+        Assert.assertEquals(expectedName, person.getName());
+    }
 }
+
