fd35df96922bd6b480d774d87fc39e2dff122c92
Michal Terranova <michalterranova@mail.com>
2020-11-20 15:43:55 -0500

    Update Instructors.java

--- src/main/java/com/github/curriculeon/Instructors.java
+++ src/main/java/com/github/curriculeon/Instructors.java
@@ -5,20 +5,20 @@ public class Instructors extends People<Instructor>{
     private static final Instructors INSTANCE = new Instructors();
 
     private Instructors() {
-        add(new Instructor(3L, "teacher1"));
-        add(new Instructor(4L, "teacher2"));
+        add(new Instructor(3L, "Teacher1"));
+        add(new Instructor(4L, "Teacher2"));
     }
 
     public static Instructors getInstance(){
         return INSTANCE;
     }
 
-
     @Override
     public Learner[] toArray() {
         Person[] newPerson = new Person[0];
-        Person[] filledPeople = personList.toArray(newPerson);
+        Learner[] filledPeople = (Learner[]) personList.toArray(newPerson);
 
         return filledPeople;
     }
+
 }
