eaed899af5ace0e7d7c857bb8ef661e63b4afdc1
Michal Terranova <michalterranova@mail.com>
2020-11-20 15:42:51 -0500

    Update Educator.java

--- src/main/java/com/github/curriculeon/Educator.java
+++ src/main/java/com/github/curriculeon/Educator.java
@@ -1,4 +1,40 @@
 package com.github.curriculeon;
 
-public class Educator {
+import com.github.curriculeon.Learner;
+import com.github.curriculeon.Teacher;
+
+public enum Educator implements Teacher {
+
+    Teacher1(1L, "Teacher1"),
+    Teacher2(2L, "Teacher2");
+
+    public final Instructor instructor;
+
+    public double getTimeWorked() {
+        return timeWorked;
+    }
+
+    public double timeWorked;
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    Educator(Long id, String name){
+        this.instructor = new Instructor(id, name);
+        Instructors.getInstance().add(instructor);
+    }
+
+
+    @Override
+    public void teach(Learner learner, double numberOfHours) {
+        timeWorked+= numberOfHours;
+        instructor.teach(learner, numberOfHours);
+    }
+
+    @Override
+    public void lecture(Learner[] learners, double numberOfHours) {
+        timeWorked+= numberOfHours;
+        instructor.lecture(learners, numberOfHours);
+    }
 }
