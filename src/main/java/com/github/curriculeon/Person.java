fa3af6f2ce03749ced9666a889cb9221f2b07749
Michal Terranova <michalterranova@mail.com>
2020-11-20 14:46:21 -0500

    Update Person.java

--- src/main/java/com/github/curriculeon/Person.java
+++ src/main/java/com/github/curriculeon/Person.java
@@ -2,4 +2,25 @@ package com.github.curriculeon;
 
 public class Person {
 
+    private final long id;
+    private String name;
+
+    public Person(long id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String setName(String name) {
+        this.name = name;
+        return name;
+    }
+    
 }
