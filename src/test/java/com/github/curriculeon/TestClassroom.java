653580cc2aa6bfce778ea9b0e06f9236be016a18
Michal Terranova <michalterranova@mail.com>
2020-11-20 15:49:15 -0500

    Update TestClassroom.java

--- src/test/java/com/github/curriculeon/TestClassroom.java
+++ src/test/java/com/github/curriculeon/TestClassroom.java
@@ -1,4 +1,37 @@
 package com.github.curriculeon;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Map;
+import java.util.Set;
+
 public class TestClassroom {
+    @Test
+    public void testHostLecutuer(){
+        //given
+        Classroom classroom = Classroom.INSTANCE;
+//        Instructors instructors = Instructors.getInstance();
+//        Students students = Students.getInstance();
+        Teacher teacherAsTeacher = (Teacher) Instructors.getInstance().findById(3L);
+        Map<Student, Double> preStudyMap = classroom.getStudyMap();
+        Double numberOfHoursToTeach = 999.0;
+        Double expectedNumberOfHoursLearned = numberOfHoursToTeach / Students.getInstance().count();
+
+        //when
+        classroom.hostLecture(teacherAsTeacher, numberOfHoursToTeach);
+        Map<Student, Double> postStudyMap = classroom.getStudyMap();
+        Set<Student> keySet = postStudyMap.keySet();
+        for(Student student: keySet){
+            Double preStudyTime = preStudyMap.get(student);
+            Double expectedStudyTime = preStudyTime + expectedNumberOfHoursLearned;
+            Double actualStudyTime = postStudyMap.get(student);
+
+            Assert.assertEquals(expectedStudyTime,actualStudyTime);
+        }
+
+        //then
+
+
+    }
 }
